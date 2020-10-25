f1d30a5d340e5851b4be6e1e069caaad9f3a17fa
KateVeselovskaya <ekaterina.veselovskaya@rokolabs.com>
2020-10-25 21:05:00 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketPairs = [];
+  bracketsConfig.forEach(item => {
+    bracketPairs.push(item.join(''));
+  });
+
+  while (bracketPairs.some(pair => str.includes(pair))) {
+    bracketsConfig.forEach(arr => {
+      let pairForDelete = arr.join('');
+       (pairForDelete);
+      while (str.includes(pairForDelete)) {
+        str = str.replace(pairForDelete, '');
+      }
+    });
+  }
+  return  str === '';
 }
